f96a6733e5d52f2abb289c8781f01addcf76bff0
nazerise <94860981+nazerise@users.noreply.github.com>
2021-12-27 18:22:01 +0330

    Update commission.sh

--- commission.sh
+++ commission.sh
@@ -2,18 +2,17 @@
 ###find machine with NEW status
 new_machines=$(maas admin machines read | jq ".[] | {hostname:.hostname, system_id: .system_id, status:.status_name}" --compact-output | grep "New")
 config_power_machines_manual() {
-    maas admin machine update $system_id_1 power_type=manual > /dev/null 2>&1
+    maas admin machine update $1 power_type=manual > /dev/null 2>&1
 }
 config_commission_machines() {
-    maas admin machine commission $system_id_1 enable_ssh=1 commissioning_scripts=ssh > /dev/null 2>&1
+    maas admin machine commission $1 enable_ssh=1 commissioning_scripts=ssh > /dev/null 2>&1
 }
 run_commission_machines() {
-	echo $1
 	system_id_1=$( echo $1 | awk -F ',' '{print $2}' | awk -F '"' '{print $4}' )
-	config_power_machines_manual 
+	config_power_machines_manual $system_id_1
 	if [ "$?" == "0" ];then
 		echo -e "\033[0;32mSuccessfully Change Power \033[0m"
-		config_commission_machines 
+		config_commission_machines $system_id_1
 		echo -e "\033[0;32mPlease Reset machines $line \033[0m"
 	else
 		echo -e "\033[0;31mFailed To Change Power \033[0m"
@@ -22,7 +21,7 @@ run_commission_machines() {
 }
 
 while
-    read line
+	read line
 do
-		run_commission_machines $line
+	run_commission_machines $line
 done <<<"$new_machines"
