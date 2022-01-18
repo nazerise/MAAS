1cd9dfe28b9e9fcfe254897d65dac98a726f52b6
nazerise <94860981+nazerise@users.noreply.github.com>
2022-01-18 12:06:52 +0330

    Update network-bond.sh

--- network-bond.sh
+++ network-bond.sh
@@ -5,14 +5,14 @@ bond_div=2
 
 vlan_check () {
         if [ "$?" = "0" ];then
-                echo -e "\033[0;32m Create Vlan $bond_vid with IP $IP_machines \033[0m"
+                echo -e "\033[0;32mCreate Vlan $bond_vid with IP $IP_machines \033[0m"
         else
-                echo -e "\033[0;31m fail to create Vlan $bond_vid \033[0m"
+                echo -e "\033[0;31mfail to create Vlan $bond_vid \033[0m"
         fi
 }
 system_values () {
 ###find ready machine's ID
-    echo -e "\033[0;31mfind ready machines ID ...\033[0m"
+    echo -e "\033[0;33mfind ready machines ID ...\033[0m"
     system_ids=$( maas admin machines read | jq '.[] | select(.status_name == "Ready") | .system_id ' | awk -F '"' '{print $2}' )
 }
 create_bond() {
@@ -29,7 +29,7 @@ create_bond() {
     do
 	if [[ $n2 == "1" ]];then
 		n2=$(($n2 + 1))
-        	maas admin interfaces create-bond $line_system_ids name=bond$bond_name bond_mode=$bond_type ${list_ints_id[@]:$n1:$n2} 1> /dev/null
+        	maas admin interfaces create-bond $line_system_ids name=bond$bond_name bond_mode=$bond_type ${list_ints_id[@]:$n1:$n2} 1> /dev/null 
 		break
 	fi
         maas admin interfaces create-bond $line_system_ids name=bond$bond_name bond_mode=$bond_type ${list_ints_id[@]:$n1:$n2} 1> /dev/null
@@ -38,21 +38,41 @@ create_bond() {
         bond_name=$(($bond_name + 1))
     done
 }
+sub_info() {
+        subset_cidr=$(maas admin subnets read | jq ".[] |  select(.vlan.id == ${list_vlan_id[$i]}) | .cidr " | awk -F '"' '{print $2}')
+        subnet_name=$(maas admin subnets read | jq ".[] |  select(.vlan.id == ${list_vlan_id[$i]}) | .name ")
+}
+config_vlan_1() {
+        vlan_subnet_1=$(echo $subset_cidr |  cut -d/ -f 1 )
+        read -e -i $vlan_subnet_1 -p "ip of $bond_vid $subnet_name: " vlan_ip_1 < /dev/tty
+        octed=$(echo $vlan_ip_1 | cut -d. -f4- )
+}
+
+config_vlan_2() {
+        vlan_subnet_2=$(echo $subset_cidr | cut -d. -f 1-3).$octed
+        read -e -i $vlan_subnet_2 -p "ip of $bond_vid $subnet_name: " input < /dev/tty
+        vlan_ip_2="${input:-$vlan_subnet_2}"
+}
 
 add_vlan() {
-    while
-        read line_vlan_id
-    do
-        subnet_vlan=$( maas admin subnets read | jq ".[] |  select(.vlan.id == $line_vlan_id) | .cidr " | awk -F '"' '{print $2}')
-        IP_machines=$(echo $subnet_vlan | cut -d. -f -3 ).5
-    	maas admin interfaces create-vlan $line_system_ids vlan=$line_vlan_id parent=$int_bond_id 1> /dev/null
-        bond_vid=$(maas admin vlans read $fabric_bond_id | jq ".[] | select(.id == $line_vlan_id) | .vid ")
+    for i in ${!list_vlan_id[@]}; do
+        sub_info
+        subnet_vlan=$( maas admin subnets read | jq ".[] |  select(.vlan.id == ${list_vlan_id[$i]}) | .cidr " | awk -F '"' '{print $2}')
+#        IP_machines=$(echo $subnet_vlan | cut -d. -f -3 ).5
+    	maas admin interfaces create-vlan $line_system_ids vlan=${list_vlan_id[$i]} parent=$int_bond_id 1> /dev/null
+        bond_vid=$(maas admin vlans read $fabric_bond_id | jq ".[] | select(.id == ${list_vlan_id[$i]}) | .vid ")
         int_type=vlan
         bond_int_id=$(maas admin interfaces read $line_system_ids | jq ".[] | select(.vlan.vid == $bond_vid and .type == \"$int_type\" ) | .id ")
+        if [[ "$i" == "0" ]]; then
+            config_vlan_1
+            IP_machines=$vlan_ip_1
+        else
+            config_vlan_2
+            IP_machines=$vlan_ip_2
+        fi
         maas admin interface link-subnet $line_system_ids $bond_int_id mode=STATIC subnet=$subnet_vlan ip_address=$IP_machines  1> /dev/null
         vlan_check
-    done<<<"$vlan_id"
-
+    done
 }
 add_bond_vlan() {
     for ((i=0;i<=$bond_name;i++));
@@ -62,6 +82,7 @@ add_bond_vlan() {
 	int_bond_name=bond$i
         int_bond_id=$(maas admin interfaces read $line_system_ids | jq ".[] | select(.name == \"$int_bond_name\") | .id" )
         vlan_id=$(maas admin vlans read $fabric_bond_id | jq '.[] | .id ')
+        list_vlan_id=( $( echo $vlan_id ) )
         add_vlan
     done
 }
@@ -71,7 +92,8 @@ while
     read line_system_ids
 do
     maas admin machines read | jq ".[] | select(.system_id==\"$line_system_ids\") | {hostname:.hostname, system_id: .system_id, status:.status_name}" --compact-output
-    int_id_1g=$(maas admin interfaces read $line_system_ids | jq ".[] |  select(.interface_speed == 1000 and .vlan.vid == 0 ) | .id " )
+    #int_id_1g=$(maas admin interfaces read $line_system_ids | jq ".[] |  select(.interface_speed == 1000 and .vlan.vid == 0 ) | .id " )
+    int_id_1g=$(maas admin interfaces read $line_system_ids | jq ".[] |  select(.interface_speed == 1000 ) | .id " )
     int_id_10g=$(maas admin interfaces read $line_system_ids | jq ".[] |  select(.interface_speed == 10000 and .vlan.vid == 0 ) | .id " )
     if [[ -n $int_id_1g ]];then
         int_id=$int_id_1g
